75e8d8e54a5d9cabf4d8618a1d16413883101758
Darren Minsoo Kim <darren_minsoo_kim@outlook.com>
2020-12-27 00:21:49 -0500

    scheme added

--- init.el
+++ init.el
@@ -42,11 +42,12 @@
 (load "_js.el") ;; javascript and typescript
 (load "_yaml.el") ;; yaml
 (load "_el.el") ;; emacs lisp
+(load "_scm.el") ;; scheme
 ;;(load "_py.el") ;; python
 ;;(load "_hy.el") ;; hy
 ;;(load "_lisp.el") ;; common lisp
 ;;(load "_cs.el") ;; csharp
-;;(load "_clj.el") ;; clojure
+(load "_clj.el") ;; clojure
 ;;(load "_rkt.el") ;; racket
 ;;(load "_fs.el") ;; fsharp
 ;;(load "_hs.el") ;; haskell
@@ -67,7 +68,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons-dired-mode dracula-theme badger-theme alect-light alect-theme parinfer lilypond-mode crm-custom ido-yes-or-no amx ido-completing-read+)))
+   (quote
+    (all-the-icons-dired-mode dracula-theme badger-theme alect-light alect-theme parinfer lilypond-mode crm-custom ido-yes-or-no amx ido-completing-read+))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- mod/lng/_clj.el
+++ mod/lng/_clj.el
@@ -1,25 +1,22 @@
 ;; https://github.com/clojure-emacs/clojure-mode
 (ensure-installed-and-require 'clojure-mode)
-
-;; https://docs.cider.mx/cider/usage/cider_mode.html
 (ensure-installed-and-require 'cider)
 (add-hook 'clojure-mode-hook #'cider-mode)
 
 ;; cider to company 
 ;; https://docs.cider.mx/cider/usage/code_completion.html
-(add-hook 'cider-repl-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'company-mode)
+;;(add-hook 'cider-repl-mode-hook #'company-mode)
+;;(add-hook 'cider-mode-hook #'company-mode)
 
 ;; flycheck
 ;;(ensure-installed-and-require 'flycheck-clojure)
 ;;(eval-after-load 'flycheck '(flycheck-clojure-setup))
 
 ;; provides minibuffer documentation for the code you're typing into the repl
-(add-hook 'cider-mode-hook 'eldoc-mode)
+;;(add-hook 'cider-mode-hook 'eldoc-mode)
 
 ;; Use clojure mode for other extensions
 (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
 
@@ -27,4 +24,5 @@
 ;; https://github.com/borkdude/clj-kondo/blob/master/doc/install.md
 ;; install clj-kondo to the os level first
 ;; https://github.com/borkdude/flycheck-clj-kondo
-(ensure-installed-and-require 'flycheck-clj-kondo)
+;;(ensure-installed-and-require 'flycheck-clj-kondo)
+
