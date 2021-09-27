cbba2aaeaed63a7b2057a21ec43304a2a23e2dbd
Darren Minsoo Kim <darren_minsoo_kim@outlook.com>
2021-09-27 10:23:33 -0400

    removed rust, sorted out font and size.

--- mod/vsl/_font.el
+++ mod/vsl/_font.el
@@ -1,5 +1,4 @@
-;;(defvar desired-font-and-size "Source Code Pro-9")
-(add-to-list 'default-frame-alist '(font . "Menlo-12"))
-(set-face-attribute 'default t :font "Menlo-12")
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-10"))
+(set-face-attribute 'default t :font "Source Code Pro-10")
 ;; increase font size for better readability
 ;; (set-face-attribute 'default nil :height 110)
