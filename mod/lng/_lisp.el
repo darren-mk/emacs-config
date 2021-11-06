93e8cdb5bcecea1a14bff46907a9327899071ae1
Minsoo Kim <minsookim@Minsoos-MacBook-Pro.local>
2021-11-07 03:49:04 +0900

    paredit added for lisp

--- mod/lng/_lisp.el
+++ mod/lng/_lisp.el
@@ -1,3 +1,9 @@
 ;; https://lisp-lang.org/learn/getting-started/
 (load (expand-file-name "~/.quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "/opt/homebrew/bin/sbcl")
+;;
+(add-hook 'lisp-mode-hook
+	  #'enable-paredit-mode)
+;; 
+(add-hook 'slime-repl-mode-hook
+	  (lambda () (paredit-mode +1)))
