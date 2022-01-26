bfda6b35dedb803003f8e97aae6bf7b1005e4c6f
Darren Minsoo Kim <darren_minsoo_kim@outlook.com>
2022-01-26 00:53:45 -0500

    detailed for sbcl

--- mod/lng/_lisp.el
+++ mod/lng/_lisp.el
@@ -1,10 +1,14 @@
 ;; https://lisp-lang.org/learn/getting-started/
-;; (load (expand-file-name "~/.quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "/opt/homebrew/bin/sbcl")
+;; load slime-helper
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+;; path for sbcl
+(setq inferior-lisp-program
+      (case system-type
+	('gnu/linux "/usr/bin/sbcl")
+	('darwin "/opt/homebrew/bin/sbcl")))
 ;; sly
 (ensure-installed-and-require 'sly)
 ;; paredit
-(add-hook 'lisp-mode-hook
-	  #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook
-	  #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'sly-repl-mode-hook (lambda () (paredit-mode +1)))
