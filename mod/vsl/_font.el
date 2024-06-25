ee6fab5a11b5519ad846beafa6840a2d14200a0a
Darren Kim <darren.minsoo.kim@gmail.com>
2024-06-25 09:59:24 -0400

    optionalize font and size

--- mod/vsl/_font.el
+++ mod/vsl/_font.el
@@ -1,4 +1,4 @@
-(add-to-list 'default-frame-alist '(font . "Menlo-12"))
-(set-face-attribute 'default t :font "Menlo-12")
+;; (add-to-list 'default-frame-alist '(font . "Menlo-12"))
+;; (set-face-attribute 'default t :font "Menlo-12")
 ;; increase font size for better readability
 ;; (set-face-attribute 'default nil :height 110)
