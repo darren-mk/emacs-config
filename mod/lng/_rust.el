2ca909eb87a7943d49f750565e875a60175993fe
Darren Kim <darren.minsoo.kim@gmail.com>
2025-09-01 17:20:41 +0300

    update settings for rust

--- mod/lng/_rust.el
+++ mod/lng/_rust.el
@@ -2,8 +2,9 @@
 ;; $ brew install rustup
 ;; $ rustup-init
 ;; $ rustup component add rust-analyzer
-
-(use-package rust-mode
-  :defer t :ensure t)
-
-(add-hook 'rust-mode-hook #'lsp)
+(use-package rustic
+  :ensure t
+  :config
+  (setq rustic-format-on-save nil)
+  :custom
+  (rustic-cargo-use-last-stored-arguments t))
