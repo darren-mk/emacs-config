d81841873c9ddd8dfd7aa1b65f7faf352f71645b
Darren Kim <darren.minsoo.kim@gmail.com>
2025-09-09 07:17:23 -0400

    update font settings for linux

--- mod/vsl/_font.el
+++ mod/vsl/_font.el
@@ -1,4 +1,19 @@
-;; (add-to-list 'default-frame-alist '(font . "Menlo-12"))
-;; (set-face-attribute 'default t :font "Menlo-12")
-;; increase font size for better readability
-;; (set-face-attribute 'default nil :height 110)
+(cond
+ ((eq system-type 'gnu/linux)
+  (let ((font-list '("Bitstream Vera Sans Mono-10"
+                     "DejaVu Sans Mono-10"
+                     "monospace-10")))
+    (catch 'found
+      (dolist (font font-list)
+        (when (find-font (font-spec :name font))
+          (add-to-list 'default-frame-alist `(font . ,font))
+          (set-face-attribute 'default t :font font :height 110)
+          (throw 'found font))))))
+ ((eq system-type 'darwin) nil))
+
+;; ----- zoom keys (buffer-local text scale) -----
+;; C-= : zoom in, C-- : zoom out, C-0 : reset
+(global-set-key (kbd "C-=") #'text-scale-increase)
+(global-set-key (kbd "C-+") #'text-scale-increase)  ;; some keyboards send C-+
+(global-set-key (kbd "C--") #'text-scale-decrease)
+(global-set-key (kbd "C-0") (lambda () (interactive) (text-scale-set 0)))
