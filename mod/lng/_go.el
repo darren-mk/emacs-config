d093bda1668a262d872f2564d8f6d31f98878040
Darren Kim <darren.minsoo.kim@gmail.com>
2025-09-20 11:23:56 -0400

    add commands for go test

--- mod/lng/_go.el
+++ mod/lng/_go.el
@@ -17,5 +17,12 @@
 (add-hook 'go-mode-hook #'my-go-mode-lsp-disable-formatting)
 
 (use-package gotest
-  :defer t
-  :ensure t)
+  :ensure t
+  :commands (go-test-current-file go-test-current-test
+             go-test-current-project go-test-current-benchmark)
+  :init
+  (with-eval-after-load 'go-mode
+    (define-key go-mode-map (kbd "C-c t f") #'go-test-current-file)
+    (define-key go-mode-map (kbd "C-c t t") #'go-test-current-test)
+    (define-key go-mode-map (kbd "C-c t p") #'go-test-current-project)
+    (define-key go-mode-map (kbd "C-c t b") #'go-test-current-benchmark)))
