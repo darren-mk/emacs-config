545ab4bfb85ae75ab115017a3d170f191fcdbe91
Darren Kim <darren.minsoo.kim@gmail.com>
2025-10-10 08:33:48 -0400

    hide prog langs by default

--- init.el
+++ init.el
@@ -16,15 +16,12 @@
  '(ignored-local-variable-values
    '((eval put 'test-js-eval 'common-lisp-indent-function 1)
      (eval put 'test-ps-js 'common-lisp-indent-function 1)
-     (eval put 'mailer-merge-common-variables 'racket-indent-function #'begin)
-     (Package . JSON)
-     (Syntax . ANSI-COMMON-LISP)
-     (Syntax . ANSI-Common-Lisp)
-     (Base . 10)
-     (Package . CL-USER)
+     (eval put 'mailer-merge-common-variables 'racket-indent-function
+           #'begin)
+     (Package . JSON) (Syntax . ANSI-COMMON-LISP)
+     (Syntax . ANSI-Common-Lisp) (Base . 10) (Package . CL-USER)
      (Syntax . COMMON-LISP)))
- '(package-selected-packages
-   '(amx apheleia cider company corfu crm-custom dockerfile-mode doom-themes eglot-fsharp exec-path-from-shell flycheck go-mode gotest graphql-mode ido-yes-or-no inf-clojure json-mode lsp-pyright lsp-treemacs lsp-ui magit mermaid-mode nerd-icons-completion nerd-icons-corfu ocamlformat orderless org-ac ox-epub paredit projectile pytest rainbow-delimiters restclient rustic solarized-theme spacemacs-theme svelte-mode utop web-mode yaml-mode yasnippet))
+ '(package-selected-packages nil)
  '(warning-suppress-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
