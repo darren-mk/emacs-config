cef2fb28c2097b1ef6e978b59d55d7ffee5feeb5
Darren Kim <darren.minsoo.kim@gmail.com>
2025-10-26 08:44:52 -0400

    separate autogen custom codes out

--- init.el
+++ init.el
@@ -1,31 +1,8 @@
-;;; Map
+;; save customize changes somewhere separate from init.el
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file 'noerror 'nomessage))
+
+;; map
 (add-to-list 'load-path "~/.emacs.d/mod")
-(load "_map.el") ;; repositories
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(eglot-confirm-server-edits nil nil nil "Customized with use-package eglot")
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-type 'cabal-repl)
- '(ignored-local-variable-values
-   '((eval put 'test-js-eval 'common-lisp-indent-function 1)
-     (eval put 'test-ps-js 'common-lisp-indent-function 1)
-     (eval put 'mailer-merge-common-variables 'racket-indent-function
-           #'begin)
-     (Package . JSON) (Syntax . ANSI-COMMON-LISP)
-     (Syntax . ANSI-Common-Lisp) (Base . 10) (Package . CL-USER)
-     (Syntax . COMMON-LISP)))
- '(package-selected-packages nil)
- '(warning-suppress-types '((lsp-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load "_map.el")
