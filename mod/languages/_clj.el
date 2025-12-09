1e5ec40fe4221ed43afdcb62facaef33950098aa
Darren Kim <darren.minsoo.kim@gmail.com>
2025-12-09 08:13:58 -0500

    rewrote clojure config with eglot

--- mod/languages/_clj.el
+++ mod/languages/_clj.el
@@ -6,51 +6,67 @@
 ;; https://clojure.org/guides/install_clojure
 ;; brew install clojure/tools/clojure
 
-;; clojure modes
-(ensure-installed-and-require 'clojure-mode)
-(ensure-installed-and-require 'inf-clojure)
+;; install clojure-lsp
+;; https://formulae.brew.sh/formula/clojure-lsp
+;; brew install clojure-lsp
 
-;; cider
-(ensure-installed-and-require 'cider)
-(add-hook 'clojure-mode-hook #'cider-mode)
-
-;; mac-os
-(ensure-installed-and-require 'exec-path-from-shell)
-(when (memq window-system '(mac ns x))
+;; mac PATH fix
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns x))
+  :ensure t
+  :config
   (exec-path-from-shell-initialize))
 
-;; other files
-(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.clj.*$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.cljc.*$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojurescript-mode))
-(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
-
 ;; paredit
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(use-package paredit
+  :ensure t)
 
-(add-hook 'clojure-mode-hook 'lsp)
-(add-hook 'clojurescript-mode-hook 'lsp)
-(add-hook 'clojurec-mode-hook 'lsp)
+;; clojure modes
+(use-package clojure-mode
+  :ensure t
+  :mode (("\\.edn\\'"  . clojure-mode)
+         ("\\.clj\\'"  . clojure-mode)
+         ("\\.cljc\\'" . clojurec-mode)
+         ("\\.cljs\\'" . clojurescript-mode)
+         ("lein-env"   . enh-ruby-mode))
+  :hook ((clojure-mode       . paredit-mode)
+         (clojurescript-mode . paredit-mode)
+         (clojurec-mode      . paredit-mode)))
 
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      treemacs-space-between-root-nodes nil
-      company-minimum-prefix-length 1
-      lsp-lens-enable t
-      lsp-signature-auto-activate nil)
+;; eglot
+(use-package eglot
+  :ensure t
+  :hook ((clojure-mode       . eglot-ensure)
+         (clojurescript-mode . eglot-ensure)
+         (clojurec-mode      . eglot-ensure))
+  :config
+  (add-to-list 'eglot-server-programs '(clojure-mode       . ("clojure-lsp")))
+  (add-to-list 'eglot-server-programs '(clojurescript-mode . ("clojure-lsp")))
+  (add-to-list 'eglot-server-programs '(clojurec-mode      . ("clojure-lsp"))))
 
-;; cider to company 
-;; https://docs.cider.mx/cider/usage/code_completion.html
-;; (add-hook 'cider-repl-mode-hook #'company-mode)
-;; (add-hook 'cider-mode-hook #'company-mode)
-;; joker
-;; (ensure-installed-and-require 'flycheck-joker)
-;; provides minibuffer documentation for the code you're typing into the repl
-;; (add-hook 'cider-mode-hook 'eldoc-mode)
-;; clj-kondo
-;; https://github.com/borkdude/clj-kondo/blob/master/doc/install.md
-;; install clj-kondo to the os level first
-;; https://github.com/borkdude/flycheck-clj-kondo
-;;(ensure-installed-and-require 'flycheck-clj-kondo)
+;; cider
+(use-package cider
+  :ensure t
+  :after clojure-mode
+  :hook ((clojure-mode    . cider-mode)
+         (cider-repl-mode . paredit-mode))
+  :custom
+  (cider-save-file-on-load t)
+  (cider-repl-display-help-banner nil))
+
+;; company
+(use-package company
+  :ensure t
+  :hook ((clojure-mode    . company-mode)
+         (cider-repl-mode . company-mode))
+  :custom
+  (company-minimum-prefix-length 1))
+
+;; flycheck + clj-kondo
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package flycheck-clj-kondo
+  :ensure t
+  :after (clojure-mode flycheck))
